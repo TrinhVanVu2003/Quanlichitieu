b71159565829a60f033a43c2453a7ce2f288d127
Trinh Van Vu <21dh114327@st,huflit,edu,vn>
2023-11-16 00:12:06 +0700

    update database

--- AppQuanLI/app/src/main/java/com/example/appquanlichitieu/DBHelper.java
+++ AppQuanLI/app/src/main/java/com/example/appquanlichitieu/DBHelper.java
@@ -12,6 +12,11 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String TEN_BANG_USER = "users";
     public static final String TEN_BANG_GIAODICH = "GiaoDich";
     public static final String TEN_BANG_DANHMUC = "DanhMuc";
+    public static final String TEN_BANG_WALLET = "Wallet";
+    public static final String TEN_BANG_SAVINGS = "Savings";
+    public static final String TEN_BANG_REPORT = "Report";
+    public static final String TEN_BANG_CURRENCY = "Currency";
+
 
     //Cot bang user
     public static final String COT_ID ="_id";
@@ -25,10 +30,40 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String COT_AMOUNT = "_amount";
     public static final String COT_ISKHOANTHU = "_iskhoanthu";
     public static final String COT_DATE = "_date";
+    public static final String COT_ID_USER_GD = "id_user_giaodich";
 
     //Cot bang danh muc
     public static final String COT_ID_DANHMUC ="id_danhmuc";
     public static final String COT_TENDANHMUC ="_tendanhmuc";
+    public static final String COT_TYPE ="_type";
+
+
+    //Cot bang vi
+    public static final String COT_WALLET_ID = "_WalletID";
+    public static final String COT_USERID ="_id_user";
+    public static final String COT_BALANCE = "_balance";
+    public static final String COT_WALLET_NAME = "_WalletName";
+
+    //Cot bảng sổ tiết kiệm
+    public static final String COT_SAVINGS_ID = "_SavingsID";
+    public static final String COT_UID = "_id_u";
+    public static final String COT_TARGET_AMOUNT = "_TargetAmount";
+    public static final String COT_CURRENT_AMOUNT ="_CurrentAmount";
+    public static final String COT_START_DATE = "_StartDate";
+    public static final String COT_END_DATE = "_EndDate";
+
+    //Cot bảng báo cáo
+    public static final String COT_REPORT_ID = "_id_report";
+    public static final String COT_USID = "_id_user_r";
+    public static final String COT_START_DATE_REPORT = "_StartDateReport";
+    public static final String COT_END_DATE_REPORT = "_EndDateReport";
+    public static final String COT_TOTAL_EXPENSIVE = "_TotalExpensive";
+    public static final String COT_TOTAL_INCOME = "_TotalIncome";
+
+    //Cot bảng currency
+    public static final String COT_CURRENCY_ID = "_CurrencyID";
+    public static final String COT_CURRENCY_CODE = "_CurrencyCode";
+    public static final String COT_CURRENCY_NAME = "_CurrencyName";
 
     public static final String TAO_BANG_USER = ""
                  + "CREATE TABLE " + TEN_BANG_USER + " ( "
@@ -44,24 +79,68 @@ public class DBHelper extends SQLiteOpenHelper {
             + COT_GHICHU + " text, "
             + COT_ISKHOANTHU + " integer, "
             + COT_DATE + " text, "
-            + COT_ID_DANHMUC + " integer, " // Thêm cột liên kết với bảng danh mục
-            + " foreign key (" + COT_ID_DANHMUC + ") REFERENCES " + TEN_BANG_DANHMUC
-            + "(" + COT_ID_DANHMUC + "));";
+            + COT_ID_DANHMUC + " integer, "
+            + COT_ID_USER_GD + "integer, "
+            + " foreign key (" + COT_ID_USER_GD + ") REFERENCES " + TEN_BANG_USER
+            + "(" + COT_ID + "))";
 
     public static final String TAO_BANG_DANHMUC = ""
             + " create table " + TEN_BANG_DANHMUC + " ( "
             + COT_ID_DANHMUC + " integer primary key autoincrement, "
+            + COT_TYPE + " text, "
             + COT_TENDANHMUC + " text );";
+
+    public static final String TAO_BANG_WALLET = ""
+            + " create table " + TEN_BANG_WALLET + " ( "
+            + COT_WALLET_ID + " integer primary key autoincrement, "
+            + COT_WALLET_NAME + " text, "
+            + COT_BALANCE + " real, "
+            + COT_USERID + " integer, "
+            + " foreign key (" + COT_USERID + ") REFERENCES " + TEN_BANG_USER
+            + "(" + COT_ID + "))";
+
+    public static final String TAO_BANG_REPORT = ""
+            + " create table " + TEN_BANG_REPORT + " ( "
+            + COT_REPORT_ID + " integer primary key autoincrement, "
+            + COT_START_DATE_REPORT + " text, "
+            + COT_END_DATE_REPORT + " text, "
+            + COT_TOTAL_EXPENSIVE + " real, "
+            + COT_TOTAL_INCOME + " real, "
+            + COT_USID + " integer, "
+            + " foreign key (" + COT_USID + ") REFERENCES " + TEN_BANG_USER
+            + "(" + COT_ID + "))";
+
+    public static final String TAO_BANG_SAVINGS = ""
+            + " create table " + TEN_BANG_SAVINGS + " ( "
+            + COT_SAVINGS_ID + " integer primary key autoincrement, "
+            + COT_TARGET_AMOUNT + " real, "
+            + COT_CURRENT_AMOUNT + " real, "
+            + COT_START_DATE + " text, "
+            + COT_END_DATE + " text, "
+            + COT_UID + " integer, "
+            + " foreign key (" + COT_UID + ") REFERENCES " + TEN_BANG_USER
+            + "(" + COT_ID + "))";
+
+    public static final String TAO_BANG_CURRENCY = ""
+            + " create table " + TEN_BANG_CURRENCY + " ( "
+            + COT_CURRENCY_ID + " integer primary key autoincrement, "
+            + COT_CURRENCY_CODE + " text, "
+            + COT_CURRENCY_NAME + " text );";
 
     public DBHelper( Context context) {
         super(context, TEN_DATABASE, null, 1);
     }
+
 
     @Override
     public void onCreate(SQLiteDatabase MyDB) {
         MyDB.execSQL(TAO_BANG_USER);
         MyDB.execSQL(TAO_BANG_GIAO_DICH);
         MyDB.execSQL(TAO_BANG_DANHMUC);
+        MyDB.execSQL(TAO_BANG_WALLET);
+        MyDB.execSQL(TAO_BANG_REPORT);
+        MyDB.execSQL(TAO_BANG_SAVINGS);
+        MyDB.execSQL(TAO_BANG_CURRENCY);
     }
 
     @Override
@@ -69,6 +148,10 @@ public class DBHelper extends SQLiteOpenHelper {
         MyDB.execSQL("DROP TABLE IF EXISTS " + TEN_BANG_USER);
         MyDB.execSQL("DROP TABLE IF EXISTS " + TEN_BANG_GIAODICH);
         MyDB.execSQL("DROP TABLE IF EXISTS " + TEN_BANG_DANHMUC);
+        MyDB.execSQL("DROP TABLE IF EXISTS " + TEN_BANG_CURRENCY);
+        MyDB.execSQL("DROP TABLE IF EXISTS " + TEN_BANG_REPORT);
+        MyDB.execSQL("DROP TABLE IF EXISTS " + TEN_BANG_WALLET);
+        MyDB.execSQL("DROP TABLE IF EXISTS " + TEN_BANG_SAVINGS);
     }
 
 }
